d4fbf70d2ffdb9c214ab7e722087d90cea5ca929
kelv1n27 <68477172+kelv1n27@users.noreply.github.com>
2020-07-20 16:11:03 -0400

    yers

--- Scraper/src/Scraper.java
+++ Scraper/src/Scraper.java
@@ -19,7 +19,7 @@ public class Scraper {
         System.out.println("Hello World!");
 
         //Instantiate a URL
-        URL webAddress = new URL("https://www.youtube.com");
+        URL webAddress = new URL("https://www.pickerington.k12.oh.us/pickerington-central-high/directory/049048051054/");
 
 //        System.out.println("URL: " + webAddress);
 
@@ -30,12 +30,12 @@ public class Scraper {
         StringBuffer strBuffer = new StringBuffer();
 
         while (scanner.hasNext()) {
-            strBuffer.append(scanner.next());
+            strBuffer.append(scanner.next() + " ");
         }
 
         String result = strBuffer.toString();
 
-        result = result.replaceAll("<[^>]*>", "");
+//        result = result.replaceAll("<[^>]*>", "");
 
 //        System.out.println("Contents: " + result);
 
@@ -44,7 +44,7 @@ public class Scraper {
     }
 
     public void parse(String t) {
-        String[] splits = t.split(" ");
+        String[] splits = t.split(" |\\(|\\)|\\:|\\;|\\<|\\>");
         for (String s : splits) {
             for (int i = 0; i < s.length(); i++) {
                 if (s.substring(i, i + 1).equals("@")) {
