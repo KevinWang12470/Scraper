8ec92852c234ab44abbcacffe31bccd2feab546e
Kevin Wang <kevin.wang.1243@gmail.com>
2020-07-20 15:06:45 -0400

    Basic url Instantiation

--- Scraper/src/Scraper.java
+++ Scraper/src/Scraper.java
@@ -1,3 +1,4 @@
+import java.net.URL;
 
 public class Scraper {
     /**
@@ -6,9 +7,11 @@ public class Scraper {
      * @param args
      *            the command line arguments; unused here
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         System.out.println("Hello World!");
+
+        URL webAddress = new URL("www.youtube.com");
 
     }
 }
