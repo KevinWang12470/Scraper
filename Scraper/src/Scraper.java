2abd62f336c0daadc5218808303fe69107bff0db
Kevin Wang <kevin.wang.1243@gmail.com>
2020-07-20 14:16:19 -0400

    Added main function, Hello World

--- Scraper/src/Scraper.java
+++ Scraper/src/Scraper.java
@@ -1,4 +1,14 @@
 
 public class Scraper {
+    /**
+     * Main method.
+     *
+     * @param args
+     *            the command line arguments; unused here
+     */
+    public static void main(String[] args) {
 
+        System.out.println("Hello World!");
+
+    }
 }
