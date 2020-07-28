4528410bc3611abc078017eeca1f424d8a6f3ebf
kelv1n27 <68477172+kelv1n27@users.noreply.github.com>
2020-07-28 17:58:16 -0400

    jar upgrade
    added stuff to make the program well as  a standalone .jar file

--- Scraper/src/Scraper.java
+++ Scraper/src/Scraper.java
@@ -1,11 +1,26 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.URL;
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 public class Scraper {
 
-    public String temp;
-
+    private String temp;
+    private String[] Exts = new String[] {".com", ".org", ".gov", ".us", ".ca"};//add more if ya feel like it
+    private File file = new File ("OUTPUT.txt");
+    FileOutputStream out;
+    
     public Scraper() {
+        try {
+			out = new FileOutputStream(file);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 
     public static void main(String[] args) throws Exception {
@@ -15,42 +30,50 @@ public class Scraper {
     }
 
     public void getURL() throws Exception {
-
-        System.out.println("Hello World!");
-
         //Instantiate a URL
-        URL webAddress = new URL("https://www.pickerington.k12.oh.us/pickerington-central-high/directory/049048051054/");
-
+        URL webAddress = new URL(JOptionPane.showInputDialog("Enter url"));
 //        System.out.println("URL: " + webAddress);
-
 //        System.out.println("Content : " + webAddress.getContent());
 
         Scanner scanner = new Scanner(webAddress.openStream());
-
         StringBuffer strBuffer = new StringBuffer();
-
         while (scanner.hasNext()) {
             strBuffer.append(scanner.next() + " ");
         }
-
         String result = strBuffer.toString();
-
 //        result = result.replaceAll("<[^>]*>", "");
-
-//        System.out.println("Contents: " + result);
-
+        System.out.println("Contents: " + result);
         this.temp = result;
 
     }
 
     public void parse(String t) {
-        String[] splits = t.split(" |\\(|\\)|\\:|\\;|\\<|\\>");
+        String[] splits = t.split(" |\\(|\\)|\\:|\\;|\\<|\\>|\\\\");
         for (String s : splits) {
-            for (int i = 0; i < s.length(); i++) {
-                if (s.substring(i, i + 1).equals("@")) {
-                    System.out.println(s);
-                }
-            }
+			/*
+			 * for (int i = 0; i < s.length(); i++) { if (s.substring(i, i + 1).equals("@"))
+			 * { System.out.println(s); } }
+			 */
+        	boolean hasAt = false;
+        	boolean hasExt = false;
+        	if (s.contains("@")) hasAt = true;
+        	for (String e:Exts) {
+        		if (s.contains(e)) {
+        			hasExt = true;
+        			break;
+        		}
+        	}
+        	if (hasAt && hasExt) {//if email
+        		System.out.println(s);
+        		try {
+					out.write((s + "\n").getBytes());
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+        	} else if (hasExt) {//if url
+//        		System.out.println(s);
+        	}
         }
     }
 
